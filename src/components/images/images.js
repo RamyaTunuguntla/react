1315f0e9da142aca9349a393ac48f6777158c83c
Ramya <163096817+RamyaTunuguntla@users.noreply.github.com>
2024-07-11 14:40:23 +0530

    hello world updated

--- src/components/images/images.js
+++ src/components/images/images.js
@@ -3,7 +3,7 @@ const Image = () => {
     return(
     <div>
         <center><img src ="https://d1ng1bucl7w66k.cloudfront.net/ghost-blog/2021/03/color-wheel.jpeg" alt= "box" width={400} height={200}></img></center>
-        
+        <h1>hello world</h1>
     </div>
     )
 }
