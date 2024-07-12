5162d2c77803e4a430e95f168ca3763998a053c6
Ramya <163096817+RamyaTunuguntla@users.noreply.github.com>
2024-07-12 13:32:24 +0530

    july11

--- src/components/ipldata/iplmatches.js
+++ src/components/ipldata/iplmatches.js
@@ -1,20 +1,30 @@
 import Ipldata from "./ipldata";
-
+import "./iplmatch.css"
 const IplItem=()=>{
     const {title,source,Trophy}=Ipldata
     return(
 
         <>
-
+        <div className="navbar">
+           <h2>Home</h2>
+           <h2>About</h2>
+           <h2>Info</h2>
+        </div>
+        <div className="grid">
         {Ipldata.map((eachIpldata)=>(
-            <center><div style={{border:"2px dashed black", color:"red",padding:"20px" ,margin:"20px 400px",background:"bisque"}}>
+            
+            <div className="card">
                     <h3>{eachIpldata.title}</h3>
                     <img src={eachIpldata.source}  width={200} height={200}/>
                    
                     <h3>Trophies: {eachIpldata.Trophy}</h3>
                 
-            </div></center>
+            </div>
+            
+    
         ))}
+            </div>
+
         </>
     )
 }
