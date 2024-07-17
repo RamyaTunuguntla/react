0cefaf4cd0e258b0f468cdaca10275c2c027bb2d
Ramya <163096817+RamyaTunuguntla@users.noreply.github.com>
2024-07-17 12:59:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
+import Data from "./components/ipl/ipldata";
 import IplItem from "./components/ipl/iplmatches";
 import Footer from "./components/ipl/footer";
- 
+import 'bootstrap/dist/css/bootstrap.min.css';
+ import CustomProgressBar from "./components/ipl/progressbar.js";
+
+
     function App (){
+      
       return(
         <>
     <IplItem></IplItem>
     <Footer></Footer>
+   
 
 
       </>
